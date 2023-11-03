da3e6b1e973865b207b7324e2310fdbe4284248b
MissKaren18 <93577516+MissKaren18@users.noreply.github.com>
2023-11-02 22:20:19 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var canvas;
+var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+
+//1. variable para la nueva imagen
+var blastImage;
+var form, player, playerCount;
+var allPlayers, car1, car2, fuels, powerCoins, obstacles;
+var cars = [];
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+  car1_img = loadImage("./assets/car1.png");
+  car2_img = loadImage("./assets/car2.png");
+  track = loadImage("./assets/track.jpg");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+
+  ///2. precargar la imagen
+  blastImage = loadImage("./assets/blast.png");
+  //3. Agrega esta imagen en el método Start() de Game.js en Car Sprites.
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.end();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
